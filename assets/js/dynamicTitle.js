e4dc34f44c1a9b37c4d9d1f4938922827313372b
buddhadeb33 <buddhadeb33@gmail.com>
2023-08-19 21:02:38 +0530

    profile photo

--- assets/js/dynamicTitle.js
+++ assets/js/dynamicTitle.js
@@ -16,14 +16,14 @@ window.onload = function () {
   document.addEventListener("visibilitychange", function (e) {
     if (!document.hidden) {
       document.title = pageTitle;
-      favicon.href = "./assets/images/dp_male.svg";
+      favicon.href = "./assets/images/buddha_main_page.jpg";
     }
   });
 
   function toggle() {
     if (document.title === attentionMessage) {
         document.title = pageTitle;
-        favicon.href = "./assets/images/dp_male.svg";
+        favicon.href = "./assets/images/buddha_main_page.jpg";
     } else {
         document.title = attentionMessage;
         favicon.href = "./assets/images/folded.png";
